678dd17b5cd613e03631cbef6a8a57eef707946b
Ma Yanlong <l.luffy.river@gmail.com>
2015-12-17 22:03:36 +0800

    fool AI

--- scripts/index.js
+++ scripts/index.js
@@ -1,73 +1,101 @@
-window.onload = function(){
-  var ROW = 15,NUM = ROW*ROW,
+window.addEventListener('load',function(){
+  var ROW = 15,
       sence = document.getElementById('sence'),
-      senceWidth = parseInt(getComputedStyle(sence).width),
-      blockOffset =(senceWidth-ROW)/ROW +'px',
-      s = true,dict1 = {},dict2 = {};
+      tips = document.getElementById('tips'),
+
+      //白棋数据 { '1_2':{x:1,y:2} }
+      whitetable = {},
+
+      //黑棋数据 { '1_2':{x:1,y:2} }
+      blacktable = {},
+
+      //ai当前落子
+      cur;
+
   //创建场景
-  (function (){
-    var el,rowline,colline;
-    for ( var i = 0;  i < ROW;  i++){
-      rowline = document.createElement('div');
-      rowline.setAttribute('class','row');
-      rowline.style.top =  (senceWidth/ROW)/2 + (senceWidth/ROW)*i + 'px';
-      colline = document.createElement('div');
-      colline.style.left = (senceWidth/ROW)/2 + (senceWidth/ROW)*i + 'px';
-      colline.setAttribute('class','col');
-      sence.appendChild(rowline);
-      sence.appendChild(colline);
-      rowline.setAttribute('class','row');
-      for ( var j = 0;  j < ROW;  j++){
-        el = document.createElement('div');
-        el.style.width =  blockOffset; el.style.height = blockOffset;
-        el.setAttribute('class','block');
-        el.setAttribute('data',i+ '_' + j);
-        sence.appendChild(el);
+  (function(){
+    var i,j,row,col,block;
+    for ( i = 0;  i < ROW;  i++){
+      row = document.createElement('div');
+      row.className = 'row';
+      row.style.top = 20 + 40*i + 'px';
+      sence.appendChild(row);
+
+      col = document.createElement('div');
+      col.className = 'col';
+      col.style.left = 20 + 40*i + 'px';
+      sence.appendChild(col);
+      for (  j = 0; j < ROW; j++){
+	block  = document.createElement('div');
+	block.className = 'block';
+	block.id = i + '_' + j;
+	sence.appendChild(block);
       }
     }
   })();
-  var  isHasWinner= function(id,dic){
-    var x = Number(id.split('_')[0]);
-    var y = Number(id.split('_')[1]);
-    var hang = 1,shu = 1, zuoxiexian = 1, youxiexian = 1;
-    var tx = x,ty = y;
-    while(dic[ tx + '_'+ (ty+1) ]){hang++;ty++;}
-    tx = x; ty = y;
-    while(dic[ tx + '_'+ (ty-1) ]){hang++;ty--;}
-    tx = x; ty = y;
-    while(dic[ (tx+1) + '_'+ ty ]){shu++;tx++;}
-    tx = x; ty = y;
-    while(dic[ (tx-1) + '_'+ ty ]){shu++;tx--;}
-    tx = x; ty = y;
-    while(dic[ (tx+1) + '_'+ (ty+1) ]){zuoxiexian++;tx++;ty++;}
-    tx = x; ty = y;
-    while(dic[ (tx-1) + '_'+ (ty-1) ]){zuoxiexian++;tx--;ty--;}
-    tx = x; ty = y;
-    while(dic[ (tx-1) + '_'+ (ty+1) ]){youxiexian++;tx--;ty++;}
-    tx = x; ty = y;
-    while(dic[ (tx+1) + '_'+ (ty-1) ]){youxiexian++;tx++;ty--;}
-    if(hang == 5 || shu == 5 || youxiexian == 5 || zuoxiexian==5) return true;
-    return false;
-  };
-  sence.onclick = function(e){
-    var el = e.target;
-    if( el == this) return;
-    if( el.hasAttribute('has-one') ) return;
 
-    var id = el.getAttribute('data');
-    if(s){
-      el.style.background = 'white';
-      el.style.boxShadow = '0 3px 10px black';
-      if( isHasWinner(id,dict1) ){alert('白赢了!');}
-      dict1[id] = true;
-      s = false;
-    }else{
-      el.style.background = '#333';
-      el.style.boxShadow = '0 3px 10px black';
-      if(isHasWinner(id,dict2)){alert('黑赢了!');}
-      dict2[id] = true;
-      s = true;
+  //ai计算落子点
+  var ai = function(){
+    var x = Math.floor(Math.random()*15);
+    var y = Math.floor(Math.random()*15);
+    while( blacktable[ x + '_'+ y ] || whitetable[ x + '_'+ y ] ){
+      x = Math.floor(Math.random()*15);
+      y = Math.floor(Math.random()*15);
     }
-    el.setAttribute('has-one','true');
+    return {x:x,y:y};
+  }
+  var xy2id  = function(x,y){
+    return x + '_' + y;
+  }
+
+  //判断是否连5
+  var  isHasWinner= function(dic,id) {
+    var x = Number(id.split('_')[0]), y = Number(id.split('_')[1]),
+	hang = 1,shu = 1, zuoxiexian = 1, youxiexian = 1,
+	tx,ty; //游标
+
+    tx = x; ty = y; while(dic[ xy2id(tx,ty+1) ]){hang++; ty++;}
+    tx = x; ty = y; while(dic[ xy2id(tx,ty-1) ]){hang++;ty--; }
+
+    tx = x; ty = y; while(dic[ xy2id(tx+1,ty) ]){shu++;tx++;}
+    tx = x; ty = y; while(dic[ xy2id(tx-1,ty) ]){shu++;tx--;}
+
+    tx = x; ty = y; while(dic[ xy2id(tx+1,ty+1) ]){zuoxiexian++;tx++;ty++;}
+    tx = x; ty = y; while(dic[ xy2id(tx-1,ty-1) ]){zuoxiexian++;tx--;ty--;}
+
+    tx = x; ty = y; while(dic[ xy2id(tx+1,ty-1) ]){youxiexian++;tx++;ty--;}
+    tx = x; ty = y; while(dic[ xy2id(tx-1,ty+1) ]){youxiexian++;tx--;ty++;}
+
+    return  (hang >= 5 || shu >= 5 || youxiexian >= 5 || zuoxiexian>=5);
   };
-};
+
+  sence.addEventListener('click',function(e){
+    if(e.target == this || !e.target.id || whitetable[e.target.id] || blacktable[e.target.id] ){
+      return false;
+    };
+    var t = e.target.id.split('_');
+    blacktable[e.target.id] = { x:t[0], y:t[1] };
+    e.target.className = 'block black';
+
+    if( isHasWinner(blacktable,e.target.id) ){
+      alert('you win');
+      return false;
+    }
+
+    var white = ai();
+    whitetable[ xy2id(white.x,white.y) ] = white;
+
+    if(cur){
+      cur.className = 'block white';
+    }
+    var el = document.getElementById(white.x + '_' + white.y );
+    el.className = 'block white current';
+    cur = el;
+
+    if( isHasWinner(whitetable,xy2id(white.x,white.y) ) ){
+      alert('computer win');
+      return false;
+    }
+  },false);
+
+},false);
